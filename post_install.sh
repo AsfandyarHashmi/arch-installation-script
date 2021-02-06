91c5ab88f6d188bfe231b34e8b3d8bdcb41ee55a
Asfandyar Hashmi <asihashmi@gmail.com>
2021-02-06 17:48:57 +0100

    Updated post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter rofi firefox mpv
+sudo pacman -S --noconfirm bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter rofi firefox mpv polybar
 
 sudo systemctl enable lightdm
 
 mkdir ~/.config
-mkdir ~/.config/bspwm
-mkdir ~/.config/polybar
-mkdir ~/.config/sxhkd
