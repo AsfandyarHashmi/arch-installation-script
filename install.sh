64c564f0b56b7a95ec4f4ab2666f78ca7a277f4a
Asfandyar Hashmi <asihashmi@gmail.com>
2021-02-06 18:34:34 +0100

    Fixed locale problem.

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ echo 'omen' > /etc/hostname
 # Region and language setup
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.conf
-#export LANG=en_US.UTF-8
+echo 'LANG=en_US.UTF-8 UTF-8' >> /etc/locale.conf
+export LANG=en_US.UTF-8
 echo 'KEYMAP=de' >> /etc/vconsole.conf
 ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc --utc
