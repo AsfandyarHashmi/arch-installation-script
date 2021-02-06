bb3cd19a8bc5be719835e740cfc6b9f429697411
Asfand <asf@pop-os.localdomain>
2021-02-06 15:35:12 +0100

    Improved

--- post_install.sh
+++ post_install.sh
@@ -2,3 +2,10 @@
 
 sudo pacman -Syyu --noconfirm
 sudo pacman -S --noconfirm bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter rofi firefox mpv
+
+sudo systemctl enable lightdm
+
+mkdir ~/.config
+mkdir ~/.config/bspwm
+mkdir ~/.config/polybar
+mkdir ~/.config/sxhkd
