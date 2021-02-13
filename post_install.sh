bc4edf7c496c4fc6c82e2abc4fa74ab471f65ead
Asfandyar Hashmi <asf@pop-os.localdomain>
2021-02-13 18:17:20 +0100

    Revamped post installation

--- post_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter rofi firefox mpv polybar
-
-sudo systemctl enable lightdm
-
-mkdir ~/.config
