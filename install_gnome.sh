00e78800942921384b69c9d3648e8215435d0aae
Asfandyar Hashmi <asihashmi@gmail.com>
2021-02-20 19:55:17 +0100

    Added gnome-passwordsafe

--- install_gnome.sh
+++ install_gnome.sh
@@ -24,7 +24,6 @@ timedatectl set-local-rtc 1 --adjust-system-clock
 # Install required packages
 pacman -S --noconfirm dhcpcd tlp tlp-rdw sudo alsa-utils alsa-plugins alsa-firmware sof-firmware alsa-ucm-conf pulseaudio pulseaudio-alsa refind gdisk networkmanager gnome gnome-tweaks firefox mpv code ntfs-3g gnome-passwordsafe
 
-
 # Debloat gnome
 pacman -R gedit gnome-boxes gnome-weather gnome-photos totem gnome-contacts gnome-calendar epiphany gnome-books gnome-documents gnome-clocks gnome-maps
 
