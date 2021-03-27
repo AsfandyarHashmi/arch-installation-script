52a0d75e2a072e87641dc461e08dd7e2cde07777
Asfandyar Hashmi <asihashmi@gmail.com>
2021-03-27 20:20:06 +0100

    Minor tweaks.

--- install_kde_minimal.sh
+++ install_kde_minimal.sh
@@ -7,9 +7,15 @@ sudo pacman -S --noconfirm plasma-desktop kscreen sddm-kcm sddm plasma-nm plasma
     dolphin konsole kdeplasma-addons spectacle mpv nomacs ark \
     partitionmanager kcalc bluedevil powerdevil qbittorrent \
     print-manager system-config-printer breeze-gtk kde-gtk-config \
-    keepassxc libnotify xclip kdegraphics-thumbnailers ffmpegthumbs
+    keepassxc libnotify xclip kdegraphics-thumbnailers ffmpegthumbs \
+    cups bluez bluez-utils avahi networkmanager tlp code
 
 sudo systemctl enable sddm 
+sudo systemctl enable cups
+sudo systemctl enable bluetooth
+sudo systemctl enable avahi-daemon
+sudo systemctl enable NetworkManager
+sudo systemctl enable tlp
 
 mkdir ~/Downloads
 mkdir -p ~/Torrents/Downloading
@@ -18,7 +24,3 @@ mkdir -p ~/Pictures/Wallpapers
 mkdir ~/Work
 mkdir ~/Projects
 mkdir ~/University
-
-sudo cp templates/etc_font_local.conf /etc/fonts/local.conf
-cp templates/Xresources ~/.Xresources
-xrdb -merge ~/.Xresources
